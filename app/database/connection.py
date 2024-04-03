4f695af081243efbdd4aa35b43f1369c0aca5e5b
hfoschiani <heitor.foschiani@outlook.com>
2024-04-02 22:15:46 -0300

    Improvement on dbconnection

--- app/database/connection.py
+++ app/database/connection.py
@@ -3,19 +3,24 @@ from sqlalchemy import create_engine
 
 
 db_connection_file_path = "C:/Users/heito/OneDrive/Heitor/Projects/Database/local_postgres_connection_information.txt"
-with open(db_connection_file_path) as f:
-    lines = f.readlines()
-
-dbhost = lines[0].strip()
-dbport = lines[1].strip()
-dbuser = lines[2].strip()
-dbpassword = lines[3].strip()
+with open(db_connection_file_path, "r") as file:
+    for line in file:
+        split_line = line.split("=")
+        if split_line[0] == "dbhost":
+            dbhost = split_line[1].strip()
+        elif split_line[0] == "dbport":
+            dbport = split_line[1].strip()
+        elif split_line[0] == "dbuser":
+            dbuser = split_line[1].strip()
+        elif split_line[0] == "dbpassword":
+            dbpassword = split_line[1].strip()
+            
 dbname = "AuthenticationManagement"
 
 
 def connect_to_postgres(connection_type="connection", host=dbhost, port=dbport, dbname=dbname, user=dbuser, password=dbpassword):
     """
-    This function create a connection with a PostgreSQL database and return the connection or the engine, as informed into "connection_type
+    This function create a connection with a PostgreSQL database and return the connection or the engine, as informed into 'connection_type'
     """
 
     if connection_type == "connection":
