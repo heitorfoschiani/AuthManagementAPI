de744440a97549c9d3e72249d2fde732028061fe
hfoschiani <heitor.foschiani@outlook.com>
2025-07-27 18:03:43 -0300

    Working with the first pytest

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+
+
+def test_register_user(client):
+    response = client.post(
+        "/auth-management/user/", 
+        headers = {"Content-type": "application/json"},
+        json = {
+            "full_name": "Bruce Wayne",
+            "email": "bruce.wayne@outlook.com",
+            "phone": "11912345678",
+            "username": "batman",
+            "password": "ImBatman",
+        }
+    )
+
+    assert response.status_code == 201
